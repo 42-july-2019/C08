5eef523c2029621c79dac977186c2d7f30a9d0f7
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-18 21:32:13 +0200

    Fix norm for ex01

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,22 +6,22 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 20:37:26 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/18 20:46:42 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/18 21:31:42 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
-#include <unistd.h>
-#define EVEN(nb) (nb % 2 == 0)
-#define SUCCESS 0
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
+# include <unistd.h>
+# define EVEN(nb) (nb % 2 == 0)
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
-typedef enum e_bool
+typedef enum	e_bool
 {
 	FALSE,
 	TRUE
-} t_bool;
+}				t_bool;
 #endif
